720ea7a8d6f880ef6ee43179c9f1f56eaa2224b4
Pingchuan Ma <pika7ma@gmail.com>
2018-05-08 17:26:48 +0800

    complete decision tree

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,1 +1,10 @@
+from DecisionTree import *
 
+
+if __name__ == '__main__':
+    id3 = ID3DecisionTree('./data/Watermelon-train1.csv', './data/Watermelon-test1.csv')
+    id3.build_tree()
+    id3.test()
+    cart = CARTDecisionTree('./data/Watermelon-train2.csv', './data/Watermelon-test2.csv')
+    cart.build_tree()
+    cart.test()
